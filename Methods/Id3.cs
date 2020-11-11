03159ecea59319cf289ac49186e769a998cced74
Max <mpolney@spirit21.com>
2020-11-11 21:43:57 +0100

    Removed the Upload to an external Hoster, Implemented the download via REST-Response, Added ID3-Tagging

---
+++ Methods/Id3.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+using Schlauchboot.Hosting.SoundCloud.Manager.Models.SoundCloud;
+
+namespace Schlauchboot.Hosting.SoundCloud.Manager.Methods
+{
+    public class Id3
+    {
+        public void SetFileMetadata(string filePath, TrackInformationMod trackInformation)
+        {
+            var fileClient = TagLib.File.Create(filePath);
+            fileClient.Tag.Title = trackInformation.title;
+            fileClient.Tag.Performers = (new List<string>() { trackInformation.artist }).ToArray();
+            fileClient.Tag.Album = trackInformation.album;
+            fileClient.Save();
+        }
+    }
+}
